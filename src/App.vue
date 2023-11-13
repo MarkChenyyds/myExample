8f92e033d4db1e509698e81b011330aaa4525cd9
Mark <chenqianmark@163.com>
2023-11-13 19:14:53 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+	<header>
+		<div class=""></div>
+	</header>
+
+	<RouterView />
+</template>
+
+<style scoped></style>
